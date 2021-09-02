92f7958fa31e2cd55135e642e5e57004c61be92b
Thiago Airold Perez <83561736+zerept@users.noreply.github.com>
2021-09-02 00:20:51 -0300

    Add files via upload

--- install-docker-mysql-security.sh
+++ install-docker-mysql-security.sh
@@ -2,11 +2,7 @@
 
 
 sudo git clone https://github.com/zerept/docker-centos-mysql-security.git
-
-sleep 5
-
+sleep 1
 cd docker-centos-mysql-security/docker-deploys/
-echo pwd
-sleep 5
-
+sleep 1
 sudo /bin/bash instalar.sh
